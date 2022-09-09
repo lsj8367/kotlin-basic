24c5d0f40b4b230bc4bbf3ddd5f494ba857b3a68
lsj8367 <lsj8367@naver.com>
2022-09-09 23:45:23 +0900

    :recycle: 연산자 오버로딩 자바버전 수정

--- src/main/kotlin/com/github/lsj8367/section2/Money.java
+++ src/main/kotlin/com/github/lsj8367/section2/Money.java
@@ -1,35 +1,20 @@
 package com.github.lsj8367.section2;
 
-import java.util.Objects;
-
 public record Money(int value) {
 
     public Money plus(final Money money) {
         return new Money(this.value + money.value);
     }
 
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        final Money money = (Money) o;
-        return value == money.value;
+    public Money minus(final Money money) {
+        compareMoneyValue(money);
+        return new Money(this.value - money.value);
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(value);
-    }
-
-    @Override
-    public String toString() {
-        return "Money{" +
-            "value=" + value +
-            '}';
+    private void compareMoneyValue(final Money money) {
+        if (this.value < money.value) {
+            throw new IllegalStateException("소비하고자 하는 돈이 더 클 수 없습니다.");
+        }
     }
 
 }
