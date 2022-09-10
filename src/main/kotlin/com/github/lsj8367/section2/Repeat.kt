b0abc18da8c23d8d50d830304a5cb47f8a87783f
lsj8367 <lsj8367@naver.com>
2022-09-10 17:59:49 +0900

    :sparkles: 반복문 구현

---
+++ src/main/kotlin/com/github/lsj8367/section2/Repeat.kt
@@ -0,0 +1,18 @@
+package com.github.lsj8367.section2
+
+fun foreach() {
+    val listOf = listOf(1, 2, 3)
+
+    for (i in listOf) {
+
+    }
+
+    for (i in 0..3 step 2) {
+
+    }
+
+    var i = 1
+    while (i <= 3) {
+
+    }
+}
